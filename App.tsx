08e2709db4742d5716f68aac687d6096700ac6e1
Erick-de-la-Villa <erick.villa@sigob.com.mx>
2024-02-26 19:50:36 -0700

    cambio de app tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Presiona el boton para mostrar una alerta</Text>
+      <Text>{"\n"}</Text>
+      <Button title="Press me" onPress={() => alert('Hello World!')} />
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
